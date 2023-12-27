9091421b49c2728aaf04a3ac3d93d308fac3ccfa
EkMakarenko <makarenkoek@yandex.ru>
2023-12-27 12:35:17 +0300

    Add

--- entrypoint.sh
+++
@@ -1,7 +0,0 @@
-until nc -z -v -w30 db 5432
-do
-  echo 'Waiting for database connection'
-  sleep 5
-done
-
-python manage.py runserver 0.0.0.0:8000
--- hotel/form.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-
-from hotel.models import Hotel
-
-
-class HotelForm(forms.ModelForm):
-    class Meta:
-        model = Hotel
-        fields = ('id', 'name', 'country', 'city', 'street', 'house_number', 'description', 'rating')
