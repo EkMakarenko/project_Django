414a576e8453ad7776161e76f2481f9e2111c973
EkMakarenko <makarenkoek@yandex.ru>
2023-12-27 16:13:09 +0300

    fix hw 22

--- hotel/views.py
+++ hotel/views.py
@@ -4,7 +4,7 @@ from django.views import generic
 
 # from comment.form import CommentForm
 from hotel.form import HotelForm
-from hotel.models import Hotel, Apartment
+from hotel.models import Hotel, Apartment, HotelApartment
 
 
 # Create your views here.
@@ -40,16 +40,31 @@ class HotelCreateView(generic.CreateView):
     template_name = 'hotel/hotel_create.html'
     form_class = HotelForm
 
+    def form_valid(self, form, apartment=None):
+        response = super().form_valid(form)
+        selected_apartment = form.cleaned_data.get('apartment')
+
+        apartment_hotel_objects = [
+            HotelApartment(hotel=self.object, apartment=apartment)
+            for apartment in selected_apartment
+        ]
+        HotelApartment.objects.bulk_create(apartment_hotel_objects)
+        return response
+
+    def get_success_url(self):
+        return reverse('hotel-list')
+
 
 class HotelUpdateView(generic.UpdateView):
     model = Hotel
     template_name = 'hotel/hotel_update.html'
     form_class = HotelForm
 
-    def get_success_url(self):
-        return reverse('hotel-list')
-
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['hotel'] = Hotel.objects.filter(is_deleted=False)
         return context
+
+    def get_success_url(self):
+        return reverse('hotel-list')
+
--- project/urls.py
+++ project/urls.py
@@ -20,5 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('hotel.urls')),
-    # path('', include('comment.urls')),
+    path('', include('comment.urls')),
 ]
